959c26df51ab1ee9ed2e658d6ea1802a11f756ba
Mitchell Stowman <mitchellstowman@gmail.com>
2023-02-20 13:25:52 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Navbar } from './comps/Navbar/navbar.comp';
+
+import './App.css';
+import './comps/Navbar/navbar.styles.scss';
+
+// Images
+import Logo from './images/logo.svg';
+import CartIcon from './images/icon-cart.svg';
+import AvatarImage from './images/image-avatar.png';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div id='navbar'>
+        <ul id='navlinks'>
+          <img id='logo' src={Logo} alt='sneakers logo'/>
+          <li>Collections</li>
+          <li>Mens</li>
+          <li>Womens</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        
+        <div id='cart-and-avatar'>
+          <div classname='cart-container'>
+            <img src={CartIcon} id='cart-icon' alt='cart icon'/>
+            <div className='cart-counter'>1</div>
+          </div>
+
+          <img src={AvatarImage} id='avatar-logo' alt='avatar'/>
+        </div>
+
+      </div>
+{/*       <Navbar /> */}
     </div>
   );
 }
---
+++ src/comps/Navbar/navbar.comp.jsx
@@ -0,0 +1,9 @@
+import '../Navbar/navbar.styles.scss';
+
+export const Navbar = () => {
+    return(
+        <div>
+            <h1>Navbar Comp</h1>
+        </div>
+    )
+}
