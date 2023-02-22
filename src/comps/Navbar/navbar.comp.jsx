8ca794e32cce9f2d71a2d33f17e4167d9869e6de
Mitchell Stowman <mitchellstowman@gmail.com>
2023-02-22 15:13:04 -0500

    - Added mobile menu - Finished mobile styling

--- src/comps/Navbar/navbar.comp.jsx
+++ src/comps/Navbar/navbar.comp.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import '../Navbar/navbar.styles.scss';
 import { CartWindow } from '../CartWindow/cart-window.component';
+import CloseIcon from '../../images/icon-close-black.svg';
 
 // Images
 import Logo from '../../images/logo.svg';
@@ -10,20 +11,46 @@ import AvatarImage from '../../images/image-avatar.png';
 
 export const Navbar = (props) => {
     const [cartState, setCartState] = useState(false);
-
     const openCart = () => {
         cartState ? setCartState(false) : setCartState(true)
     }
 
+    const [openMobileMenu, setMobileMenu] = useState(false);
+    const handleMobileMenu = () => {
+        openMobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+    }
+
     return(
         <div id='navbar'>
+
+            <div id='hamburger' onClick={handleMobileMenu}>
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+            </div>
+
+        { openMobileMenu ?
+            <div id='mobile-navbar'>
+                <div className='page-filter'></div>
+                <ul id='mobile-navlinks'>
+                    <img onClick={handleMobileMenu} src={CloseIcon} className='mobile-x'/>
+                    <li className='mobile-link'>Collections</li>
+                    <li className='mobile-link' >Mens</li>
+                    <li className='mobile-link' >Womens</li>
+                    <li className='mobile-link' >About</li>
+                    <li className='mobile-link' >Contact</li>
+                </ul>
+            </div>
+        : null    
+        }
+
             <ul id='navlinks'>
-            <img id='logo' src={Logo} alt='sneakers logo'/>
-            <li>Collections</li>
-            <li>Mens</li>
-            <li>Womens</li>
-            <li>About</li>
-            <li>Contact</li>
+                <img id='logo' src={Logo} alt='sneakers logo'/>
+                <li className='desktop-link'>Collections</li>
+                <li className='desktop-link'>Mens</li>
+                <li className='desktop-link'>Womens</li>
+                <li className='desktop-link'>About</li>
+                <li className='desktop-link'>Contact</li>
             </ul>
             
             <div id='cart-and-avatar'>
